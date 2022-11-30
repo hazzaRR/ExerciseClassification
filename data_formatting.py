9fcc4b0fc22af60012a2a5accffa6a94fb8a9c4a
hazzaRR <68497855+hazzaRR@users.noreply.github.com>
2022-11-30 13:13:39 +0000

    created formatted data instances for the additional 4 deadlift data instances, to use in the last prototype experiement

--- data_formatting.py
+++ data_formatting.py
@@ -79,7 +79,8 @@ for movement in movements:
             gyro_data = trim_dataset(gyro_data)
         
 
-            combine_data(accel_data, gyro_data, movement, instance_number, "./Prototype/data/data_instance_normalised")
+            combine_data(accel_data, gyro_data, movement, instance_number, "./Prototype/Prototype_1/data/data_instance")
+            combine_data(accel_data, gyro_data, movement, instance_number, "./Prototype/Prototype_2/data/data_instance_normalised")
 
             instance_number+= 1
 
