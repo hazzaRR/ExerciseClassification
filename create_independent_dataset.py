327db5bda4bfd391acaac94b4ba91a2be46c019e
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2023-04-27 20:10:35 +0100

    created train test dataset, with training data of one person and the test data of another persons data

---
+++ create_independent_dataset.py
@@ -0,0 +1,84 @@
+"""
+Author: Harry Redman
+Created on: 27/04/23
+Description: [takes csv files of each data instance and creates datasets in a .ts file format so that it can be used with classifiers]
+"""
+import pandas as pd
+import numpy as np
+import os
+from sktime.datasets._data_io import write_ndarray_to_tsfile
+
+
+def create_ts_file(path_to_save):   
+    CURRENT_PATH = os.getcwd()
+
+    rootdir = os.path.join(CURRENT_PATH, "Data", "formatted_data")
+
+    
+    X_train = []
+    y_train = []        
+    X_test = []
+    y_test = []         
+
+
+    for participant in os.listdir(rootdir):
+    
+        current_participant_dir = os.path.join(rootdir, participant)
+
+        for movement in os.listdir(current_participant_dir):
+
+            current_movement_dir = os.path.join(current_participant_dir, movement)
+
+
+            for data_instance in os.listdir(current_movement_dir):
+                class_label = data_instance.split('_')
+                data_instance = pd.read_csv(os.path.join(current_movement_dir, data_instance))
+                data_instance = data_instance.drop(columns=['Timestamps (ms)'])
+                data_instance = np.transpose(data_instance)
+
+                if participant == "Harry":
+                    X_train.append(data_instance)
+                    y_train.append(class_label[0])
+                else:
+                    X_test.append(data_instance)
+                    y_test.append(class_label[0])
+
+
+
+    X_train = np.asarray(X_train)
+    X_test = np.asarray(X_test)
+    y_train = np.asarray(y_train)
+    y_test = np.asarray(y_test)
+
+    # """ get the unique class labels """
+    class_labels = set(np.concatenate((y_train, y_test), axis=None))
+
+    print(class_labels)
+
+    """ create ts file for a train and test split """
+    write_ndarray_to_tsfile(data=X_train, path=f'{path_to_save}/independent', problem_name="person_independent", class_label=class_labels,
+    class_value_list=y_train, equal_length=True, series_length=100, fold="_TRAIN")
+
+    write_ndarray_to_tsfile(data=X_test, path=f'{path_to_save}/independent', problem_name="person_independent", class_label=class_labels,
+    class_value_list=y_test, equal_length=True, series_length=100, fold="_TEST")
+
+
+def main():
+    CURRENT_PATH = os.getcwd()
+    path_to_save = os.path.join(CURRENT_PATH, "Data", "datasets")
+
+    if not os.path.exists(path_to_save):
+        os.makedirs(path_to_save)
+
+
+
+    create_ts_file(path_to_save=path_to_save)
+
+    
+    
+
+
+
+if __name__ == "__main__":
+    main()
+
